ed306ba4843f072f6e76c6fd5474749e0cc98644
preenachandi <120080294+preenachandi@users.noreply.github.com>
2025-01-22 21:32:05 +0530

    setup router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./component/Navbar";
 import './index.css'; 
 function App() {
   return (
-<div>
-
-  <Navbar/>
- 
-</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
